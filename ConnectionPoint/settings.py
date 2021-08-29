e2d80792d88a00410b7d8ca6fcad7911587ffa6f
gkundwa2-ai <59675047+gkundwa2-ai@users.noreply.github.com>
2021-08-29 15:04:43 -0500

    added the datatbase connect and dj_database_url

--- ConnectionPoint/settings.py
+++ ConnectionPoint/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import django_heroku
 import os
 from pathlib import Path
@@ -74,6 +75,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
