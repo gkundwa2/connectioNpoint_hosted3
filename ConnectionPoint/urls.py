226a2845b66b96de57a124504a23cf8de24517ca
gkundwa2 <44281356+gkundwa2@users.noreply.github.com>
2021-09-07 17:30:57 +0200

    Changed django site admin

--- ConnectionPoint/urls.py
+++ ConnectionPoint/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from ConnectionApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('pastor/admin/', admin.site.urls),
     path('connectionpantry/', include('ConnectionApp.urls')),
     path('account/', include('users_app.urls')),
     path("", views.homepage),
