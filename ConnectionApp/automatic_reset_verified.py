76e4c11d0c54f7764322b984a788259e5a386523
gkundwa2-ai <59675047+gkundwa2-ai@users.noreply.github.com>
2021-09-14 21:18:47 -0500

    the reset button is now working

--- ConnectionApp/automatic_reset_verified.py
+++ ConnectionApp/automatic_reset_verified.py
@@ -5,7 +5,7 @@ from datetime import date, datetime
 def auto_reset():
 
     today = datetime.today()
-    if (today.weekday() == 1) and (today.hour >= 21 and today.minute >= 16):
+    if (today.weekday() == 4) and (today.hour >= 17 and today.minute >= 1):
 
         families = FamilyIdentity.objects.all()
         for family in families:
